b215b3a80c572afed28abb291a95ae4669f86393
muzammil-max <muzammilirfan24@gmail.com>
2024-03-31 01:55:23 +0500

    CLI gameL

---
+++ index.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const Mymsg = "                                                        Muzammil's Number Guessing game                        ";
+chalkAnimation.rainbow(Mymsg);
+setTimeout(() => {
+    console.log(chalk.bgGreen("Guess a number between 1 to 10"));
+}, 7000);
+const random = Math.floor(Math.random() * 10 + 1);
+let flag = true;
+while (flag) {
+    const answer = await inquirer.prompt([
+        {
+            name: "userGuessedNumber",
+            type: "number",
+            message: "Your Number",
+        },
+    ]);
+    if (answer.userGuessedNumber > 10) {
+        console.log(chalk.bgRed("Invalid number"));
+        flag = false;
+    }
+    else if (answer.userGuessedNumber < 0) {
+        console.log(chalk.bgRed("Invalid number"));
+        flag = false;
+    }
+    else if (answer.userGuessedNumber === random) {
+        console.log(chalk.greenBright("Nice Guess!!"));
+        flag = false;
+    }
+    else {
+        console.log(chalk.redBright("You guessed a wrong number."));
+    }
+}
